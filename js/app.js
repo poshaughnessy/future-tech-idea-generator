3c7a431df09b045279690b49b0972085147d8444
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2012-10-20 15:57:01 +0100

    Putting things in functions - trying to do multiple text per spinner

--- js/app.js
+++ js/app.js
@@ -1,117 +1,151 @@
-$(function() {
+function Generator() {
 
-    var scene = new THREE.Scene();
+    console.log('Hello?');
 
+    var scene;
+    var camera;
+    var renderer;
+    var stats;
 
-    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 10, 10000 );
+    var spinner1;
+    var spinner2;
+    var spinner3;
 
-    camera.position.z = 1200;
-
-    camera.lookAt( new THREE.Vector3(0, 0, 0) );
-
-    scene.add( camera );
-
-    var cylGeo = new THREE.CylinderGeometry(
-        100, // radiusTop
-        100, // radiusBottom
-        500, // height
-        50, // radiusSegments
-        50, // heightSegments
-        false // openEnded
-    );
-
-    var material1 = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'textures/cylinder3.jpg' ) } );
-    var material2 = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'textures/cylinder3.jpg' ) } );
-    var material3 = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'textures/cylinder3.jpg' ) } );
+    var spinner1Phrases = [
+        'Gesture-controlled',
+        'Voice-controlled',
+        'Adaptive',
+        'Responsive'
+    ];
 
 
-    var cylinder1 = new THREE.Mesh( cylGeo, material1 );
-    var cylinder2 = new THREE.Mesh( cylGeo, material2 );
-    var cylinder3 = new THREE.Mesh( cylGeo, material3 );
+    function init() {
 
-    cylinder1.position.x = -505;
-    cylinder3.position.x = 505;
+        console.log('Init');
 
-    cylinder1.rotation.z = Math.PI / 2;
-    cylinder2.rotation.z = Math.PI / 2;
-    cylinder3.rotation.z = Math.PI / 2;
+        scene = new THREE.Scene();
 
-    cylinder1.rotation.x = 0
-    cylinder2.rotation.x = 0.1;
-    cylinder3.rotation.x = 0.2;
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 10, 10000 );
+        camera.position.z = 1200;
+        camera.lookAt( new THREE.Vector3(0, 0, 0) );
 
-    // Text
-    var text3d = new THREE.TextGeometry( 'Immersive', {
+        scene.add( camera );
 
-        size: 50,
-        height: 1,
-        curveSegments: 2,
-        font: 'helvetiker'
+        var cylinder = new THREE.CylinderGeometry(
+            100, // radiusTop
+            100, // radiusBottom
+            500, // height
+            50, // radiusSegments
+            50, // heightSegments
+            false // openEnded
+        );
 
-    });
+        var material = new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'textures/cylinder3.jpg' ) } );
 
-    text3d.computeBoundingBox();
+        spinner1 = new THREE.Mesh( cylinder, material );
+        spinner2 = new THREE.Mesh( cylinder, material );
+        spinner3 = new THREE.Mesh( cylinder, material );
 
-    console.log( text3d.boundingBox );
+        spinner1.position.x = -505;
+        spinner3.position.x = 505;
 
-    var centerOffset = -0.5 * ( text3d.boundingBox.min.x - text3d.boundingBox.max.x );
+        spinner1.rotation.z = Math.PI / 2;
+        spinner2.rotation.z = Math.PI / 2;
+        spinner3.rotation.z = Math.PI / 2;
 
-    var textMaterial = new THREE.MeshBasicMaterial( { color: 0x111111, overdraw: true } );
-    text = new THREE.Mesh( text3d, textMaterial );
+        spinner1.rotation.x = 0
+        spinner2.rotation.x = 0.1;
+        spinner3.rotation.x = 0.2;
 
-    text.doubleSided = false;
+        for( var i=0; i < spinner1Phrases.length; i++ ) {
 
-    text.position.y = centerOffset;
-    text.position.z = 100;
+            var text = generateText(spinner1Phrases[i], (Math.PI * 2 * i / spinner1Phrases.length));
+            spinner1.add( text );
 
-    text.rotation.z = -Math.PI / 2;
+        }
 
-    cylinder2.add( text );
+        scene.add( spinner1 );
+        scene.add( spinner2 );
+        scene.add( spinner3 );
 
+        var ambientLight = new THREE.AmbientLight( 0x333333 );
+        scene.add( ambientLight );
 
+        var spotlight = new THREE.SpotLight(0xFFFFFF, 0.5, 2000);
+        spotlight.position.set( 0, 0, 2000 );
+        spotlight.target.position.set( 0, 0, 0 );
+        scene.add( spotlight );
 
-    scene.add( cylinder1 );
-    scene.add( cylinder2 );
-    scene.add( cylinder3 );
+        renderer = new THREE.WebGLRenderer({antialias: false, clearColor: 0x000000});
 
-    var ambientLight = new THREE.AmbientLight( 0x333333 );
-    scene.add( ambientLight );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.clear();
 
-    var spotlight = new THREE.SpotLight(0xFFFFFF, 0.5, 2000);
-    spotlight.position.set( 0, 0, 2000 );
-    spotlight.target.position.set( 0, 0, 0 );
-    scene.add( spotlight );
+        $('body').append( $(renderer.domElement) );
 
-    var renderer = new THREE.WebGLRenderer({antialias: false, clearColor: 0x000000});
+        stats = new Stats();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.clear();
+        // Align top-left
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+        stats.domElement.style.right = '0px';
 
-    $('body').append( $(renderer.domElement) );
+        $('body').append( stats.domElement );
 
-    var stats = new Stats();
+        animate();
 
-    // Align top-left
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    stats.domElement.style.right = '0px';
+    }
 
-    $('body').append( stats.domElement );
-
-    var animate = function() {
+    function animate() {
 
         requestAnimationFrame( animate );
 
-        cylinder1.rotation.x += 0.1;
-        cylinder2.rotation.x += 0.1;
-        cylinder3.rotation.x += 0.1;
+        spinner1.rotation.x += 0.1;
+        spinner2.rotation.x += 0.1;
+        spinner3.rotation.x += 0.1;
 
         renderer.render(scene, camera);
 
         stats.update();
 
-    };
+    }
 
-    animate();
+    function generateText(string, rotation) {
 
+        var text3d = new THREE.TextGeometry( string, {
+
+            size: 50,
+            height: 1,
+            curveSegments: 2,
+            font: 'helvetiker'
+
+        });
+
+        text3d.computeBoundingBox();
+
+        var centerOffset = -0.5 * ( text3d.boundingBox.min.x - text3d.boundingBox.max.x );
+
+        var textMaterial = new THREE.MeshBasicMaterial( { color: 0x111111, overdraw: true } );
+
+        var text = new THREE.Mesh( text3d, textMaterial );
+
+        text.doubleSided = false;
+
+        text.position.y = centerOffset;
+        text.position.z = 100;
+
+        text.position.x = rotation * 100;
+
+        text.rotation.z = -Math.PI / 2;
+
+        return text;
+
+    }
+
+    init();
+
+}
+
+$(function() {
+    new Generator();
 });
